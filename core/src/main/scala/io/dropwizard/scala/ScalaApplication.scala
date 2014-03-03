c951755d24dc3db9b250504c0e8082da2cdc55bc
Nick Telford <nick.telford@gmail.com>
2014-03-03 16:44:47 +0000

    Flesh out ScalaDoc on ScalaApplication

--- core/src/main/scala/io/dropwizard/scala/ScalaApplication.scala
+++ core/src/main/scala/io/dropwizard/scala/ScalaApplication.scala
@@ -3,7 +3,27 @@ package io.dropwizard.scala
 import io.dropwizard.setup.Bootstrap
 import io.dropwizard.{Application, Configuration}
 
-/** Base class for Dropwizard Services built in Scala. */
+/** Base class for Dropwizard Services built in Scala.
+  *
+  * Building a Dropwizard Application in Scala has never been easier:
+  * {{{
+  * import io.dropwizard.setup.{Bootstrap, Environment}
+  * import io.dropwizard.scala.ScalaApplication
+  *
+  * object MyApplication extends ScalaApplication[MyConfiguration] {
+  *   def init(bootstrap: MyConfiguration) {
+  *     // (optional) initialization code goes here
+  *   }
+  *
+  *   def run(configuration: MyConfiguration, environment: Environment) {
+  *     // application code goes here
+  *   }
+  * }
+  * }}}
+  *
+  * Applications derived from this trait will automatically have the
+  * [[io.dropwizard.scala.ScalaBundle]] added to them during initialization.
+  **/
 trait ScalaApplication[A <: Configuration] extends Application[A] {
 
   /** Entry point for this Dropwizard [[io.dropwizard.Application]].
