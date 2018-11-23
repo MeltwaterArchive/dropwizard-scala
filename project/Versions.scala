1d901898d02326e0899b7f11fc22aa0656fcb25b
Nick Telford <nick.telford@gmail.com>
2018-11-23 16:38:58 +0000

    Upgrade to Dropwizard 1.3.7
    Also upgrade to the latest Jackson version due to a CVE.

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,8 +1,8 @@
 
 object Versions {
 
-  val dropwizard = "1.3.1"
-  val jackson = "2.9.5"
+  val dropwizard = "1.3.7"
+  val jackson = "2.9.7"
   val mockito = "2.7.12"
   val scalaTest = "3.0.3"
 
