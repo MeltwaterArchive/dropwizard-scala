e6b38599e60d7aa6bd6a96df602a74850a3fe31a
Sean C. Sullivan <github@seansullivan.com>
2021-03-14 22:48:08 -0700

    bump scalatest, mockito-scala

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,8 +3,8 @@ object Versions {
 
   val dropwizard = "1.3.7"
   val jackson = "2.9.10"
-  val scalaTest = "3.2.3"
-  val mockitoScala = "1.16.12"
+  val scalaTest = "3.2.6"
+  val mockitoScala = "1.16.29"
 
 }
 
